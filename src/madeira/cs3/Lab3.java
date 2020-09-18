2f6b1a0081d0b81d9ebc361e2fb2eefb45825e76
Saanvi Paladugu <saanvipaladugu@saanvis-mbp.lan>
2020-09-18 02:11:03 -0400

    my third commit

--- src/madeira/cs3/Lab3.java
+++ src/madeira/cs3/Lab3.java
@@ -1,21 +1,18 @@
-package madeira.cs;
+package madeira.cs3;
 
 public class Lab3 {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
 
-
-
-
-
+        /**
+         * Author: SAANVI_PALADUGU
+         * 9/17/2020
+         **/
 
 
 
 
     }
 
-
-
-
-
-
 }
+
+
