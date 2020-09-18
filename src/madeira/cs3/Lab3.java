13b269e6b0846674ef7830b8b69d6edbab4e3ce1
Saanvi Paladugu <saanvipaladugu@saanvis-mbp.lan>
2020-09-18 03:10:30 -0400

    new

--- src/madeira/cs3/Lab3.java
+++ src/madeira/cs3/Lab3.java
@@ -1,7 +1,7 @@
 package madeira.cs3;
 
-public class Lab3 {
-    public static void main(String [] args) {
+import java.util.Scanner;
+
 
         /**
          * Author: SAANVI_PALADUGU
@@ -9,9 +9,30 @@ public class Lab3 {
          **/
 
 
+        public class Lab3 {
+            public static void main(String [] args) {
+
+                // Part I code below
+
+                Scanner scan = new Scanner(Sytem.in);
+                System.out.print("Enter your age: ");
+                int Age = scan.nextint();
+                 if(Age >= 16) {
+                     System.out.println("Congrats! You are permitted to drive!");
+                 }
+                 if(Age >= 18) {
+                     System.out.println("Congrats! You are permitted to get a tattoo!");
+                 }
+                 if(Age >= 35) {
+                     System.out.println("Congrats! You are permitted to run for President of the United States!");
+                 }
+
+                 // Part II code below
+
 
 
     }
+
 
 }
 
